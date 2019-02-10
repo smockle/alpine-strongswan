e2c65263fe2ad8492b9d9d6252aefd60cfba896b
Clay Miller <clay@smockle.com>
2019-02-10 12:58:16 -0800

    Run as docker user

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,8 @@ ENV O "strongSwan"
 ENV CN "strongSwan Root CA"
 ENV SERVER_CN "moon.strongswan.org"
 ENV USER_CN "carol@strongswan.org"
+ENV PUID 1000
+ENV PGID 1000
 
 # Copy ipsec script and make it executable
 COPY ipsec.sh /usr/local/bin/ipsec.sh
@@ -17,6 +19,22 @@ RUN chmod +x /usr/local/bin/ipsec.sh
 
 # List ports which should be published
 EXPOSE 500/udp 4500/udp
+
+# Create docker user & group
+RUN apk add --no-cache shadow sudo && \
+  if [ -z "$(getent group "${PGID}")" ]; then \
+  addgroup -S -g "${PGID}" docker; \
+  else \
+  groupmod -n docker "$(getent group "${PGID}" | cut -d: -f1)"; \
+  fi && \
+  if [ -z "$(getent passwd "${PUID}")" ]; then \
+  adduser -S -u "${PUID}" -G docker -s /bin/sh docker; \
+  else \
+  usermod -l docker -g "${PGID}" -d /home/docker -m "$(getent passwd "${PUID}" | cut -d: -f1)"; \
+  fi && \
+  echo "docker ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/docker && \
+  chmod 0440 /etc/sudoers.d/docker
+USER docker:docker
 
 # Start IPSEC VPN server & client
 ENTRYPOINT ["/usr/local/bin/ipsec.sh"]
--- ipsec.sh
+++ ipsec.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # using $C, $O, $CA_CN, $SERVER_CN, $USER_CN
 create_certs() {
   # Create IPSEC cofiguration directories
-  mkdir -p \
+  sudo mkdir -p \
     /etc/ipsec.d/private \
     /etc/ipsec.d/cacerts \
     /etc/ipsec.d/certs \
@@ -14,48 +14,48 @@ create_certs() {
     /etc/ipsec.d/crls
 
   # CA: Create private key
-  ipsec pki --gen --type rsa --size 4096 \
-    --outform pem > /etc/ipsec.d/private/caKey.pem
-  chmod 600 /etc/ipsec.d/private/caKey.pem
+  sudo ipsec pki --gen --type rsa --size 4096 \
+    --outform pem | sudo tee /etc/ipsec.d/private/caKey.pem
+  sudo chmod 600 /etc/ipsec.d/private/caKey.pem
 
   # CA: Create self-signed root certificate, using private key
-  ipsec pki --self --ca --lifetime 3650 \
+  sudo ipsec pki --self --ca --lifetime 3650 \
     --in /etc/ipsec.d/private/caKey.pem --type rsa \
     --dn "C=$C, O=$O, CN=$CA_CN" \
-    --outform pem > /etc/ipsec.d/cacerts/caCert.pem
+    --outform pem | sudo tee /etc/ipsec.d/cacerts/caCert.pem
 
   # Server: Create private key
-  ipsec pki --gen --type rsa --size 2048 \
-    --outform pem > /etc/ipsec.d/private/serverKey.pem  
-  chmod 600 /etc/ipsec.d/private/serverKey.pem
+  sudo ipsec pki --gen --type rsa --size 2048 \
+    --outform pem | sudo tee /etc/ipsec.d/private/serverKey.pem  
+  sudo chmod 600 /etc/ipsec.d/private/serverKey.pem
 
   # Server: Create CA-signed certificate, using private keys
-  ipsec pki --issue --lifetime 1095 \
+  sudo ipsec pki --issue --lifetime 1095 \
     --in /etc/ipsec.d/private/serverKey.pem --type priv \
     --cacert /etc/ipsec.d/cacerts/caCert.pem \
     --cakey /etc/ipsec.d/private/caKey.pem \
     --dn "C=$C, O=$O, CN=$SERVER_CN" \
     --san="$SERVER_CN" \
     --flag serverAuth --flag ikeIntermediate \
-    --outform pem > /etc/ipsec.d/certs/serverCert.pem
+    --outform pem | sudo tee /etc/ipsec.d/certs/serverCert.pem
 
   # User: Create a private key
-  ipsec pki --gen --type rsa --size 2048 \
-    --outform pem > /etc/ipsec.d/private/userKey.pem
-  chmod 600 /etc/ipsec.d/private/userKey.pem
+  sudo ipsec pki --gen --type rsa --size 2048 \
+    --outform pem | sudo tee /etc/ipsec.d/private/userKey.pem
+  sudo chmod 600 /etc/ipsec.d/private/userKey.pem
 
   # User: Create CA-signed certificate, using private keys
-  ipsec pki --issue --lifetime 1095 \
+  sudo ipsec pki --issue --lifetime 1095 \
     --in /etc/ipsec.d/private/userKey.pem --type priv \
     --cacert /etc/ipsec.d/cacerts/caCert.pem \
     --cakey /etc/ipsec.d/private/caKey.pem \
     --dn "C=$C, O=$O, CN=$USER_CN" \
     --san="$USER_CN" \
-    --outform pem > /etc/ipsec.d/certs/userCert.pem
+    --outform pem | sudo tee /etc/ipsec.d/certs/userCert.pem
 
   # User: Merge private key & certificate into p12 binary file
   USER_PASS=$(date +%s | sha256sum | base64 | head -c 32; echo)
-  openssl pkcs12 -export \
+  sudo openssl pkcs12 -export \
     -inkey /etc/ipsec.d/private/userKey.pem \
     -in /etc/ipsec.d/certs/userCert.pem \
     -name "$USER_CN" \
@@ -66,15 +66,21 @@ create_certs() {
   echo "p12 binary file generated for '${USER_CN}' with password '${USER_PASS}'"
 }
 
-# Create certificates if they don’t already exist 
+# Create certificates if they don’t already exist
 [ ! -f /etc/ipsec.d/userCert.p12 ] && create_certs
 unset create_certs
 
 # Replace environment variables in ipsec.conf
-eval "tee /etc/ipsec.conf << EOF
+eval "sudo tee /etc/ipsec.conf << EOF
 $(cat < /etc/ipsec.conf)
 EOF
 " 2>/dev/null
 
+# Fix volume permissions
+sudo chown docker:docker /etc/strongswan.conf
+sudo chown docker:docker /etc/ipsec.conf
+sudo chown docker:docker /etc/ipsec.secrets
+sudo chown -R docker:docker /etc/ipsec.d
+
 # Pass arguments through to ipsec
-ipsec "$@"
+sudo ipsec "$@"
